a3274e55f123d8716d18b41a768923b278e86be0
Muhammad Maghribi <muhampedia.id@gmail.com>
2023-05-17 16:09:24 +0700

    fix view

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed" dir="ltr" data-theme="theme-default"
-    data-assets-path="{{ asset('assets/') }}" data-template="vertical-menu-template">
+    data-assets-path="assets/" data-template="vertical-menu-template">
 
 <head>
     <meta charset="utf-8" />
