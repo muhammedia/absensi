a6bc15395e1a8fd34201ab3263b7700959498ba2
Fransisco <102639215+FransisGIT@users.noreply.github.com>
2023-05-17 17:00:11 +0700

    database absensi&pembayaran siswa

---
+++ database/migrations/2023_05_17_094738_create_absensi_siswas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absensi_siswas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('absen');
+            $table->integer('induk');
+            $table->string('nm_siswa');
+            $table->string('keterangan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absensi_siswas');
+    }
+};
