81f119917ef9827d8f6188b346d89c6a955977cd
Fransisco <102639215+FransisGIT@users.noreply.github.com>
2023-05-17 17:46:08 +0700

    perubahan table absensi

--- database/migrations/2023_05_17_094738_create_absensi_siswa_table.php
+++ database/migrations/2023_05_17_094738_create_absensi_siswa_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('absensi_siswas', function (Blueprint $table) {
+        Schema::create('absensi_siswa', function (Blueprint $table) {
             $table->id();
             $table->integer('absen');
             $table->integer('induk');
